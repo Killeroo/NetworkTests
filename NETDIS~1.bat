de4eebf9cd3d68ae36f5f875b6fec6710e5cd346
Killeroo <matthewcarney64@gmail.com>
2014-11-29 16:10:50 +0000

    Create NETDIS~1.bat
    Simple Batch file used to execute NetDisplay script with proper window size to avoid formating errors

---
+++ NETDIS~1.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:SubMain
+echo NOTE: Set Execution Policy to RemoteSigned in Powershell first before
+echo       attempting to start NetDisplay
+echo       Type: "Set-ExecutionPolicy RemoteSigned" (usually has to be done
+echo       in Administration Mode in Powershell: type "powershell" into the
+echo       Windows start menu and right click "Run As Administrator")
+systeminfo > %cd%\SysInfo.txt
+pause & goto SubExecute
+goto:eof
+
+:SubExecute
+title NetDisplay
+mode con: lines=24 cols=100
+cls & @Powershell.exe -File .\NETDIS~1.ps1
+goto SubExecute
+goto:eof
