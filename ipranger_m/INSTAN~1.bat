78d4ec0ab9d0f9b25bfa999a5cc05788caafa6e9
Matthew Carney <matthewcarney64@gmail.com>
2015-04-24 00:53:21 +0100

    Update INSTAN~1.bat

--- ipranger_m/INSTAN~1.bat
+++ ipranger_m/INSTAN~1.bat
@@ -1,5 +1,14 @@
 @echo off
 
+:SubMain
+echo NOTE: Set Execution Policy to RemoteSigned in Powershell first before
+echo       attempting to start NetDisplay
+echo       Type: "Set-ExecutionPolicy RemoteSigned" (usually has to be done
+echo       in Administration Mode in Powershell: type "powershell" into the
+echo       Windows start menu and right click "Run As Administrator")
+pause & goto SubExecute
+goto:eof
+
 :subExecute
 mode con: lines= cols=70
 REM Change name for SED prep
