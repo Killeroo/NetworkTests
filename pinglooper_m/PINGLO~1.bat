f0c9da56276890bc45e71490820d01511f39eb8e
Matthew Carney <matthewcarney64@gmail.com>
2015-04-24 01:02:06 +0100

    Update PINGLO~1.bat

--- pinglooper_m/PINGLO~1.bat
+++ pinglooper_m/PINGLO~1.bat
@@ -1,6 +1,15 @@
 @ECHO OFF
 
-:subMain
+:SubMain
+echo NOTE: Set Execution Policy to RemoteSigned in Powershell first before
+echo       attempting to start NetDisplay
+echo       Type: "Set-ExecutionPolicy RemoteSigned" (usually has to be done
+echo       in Administration Mode in Powershell: type "powershell" into the
+echo       Windows start menu and right click "Run As Administrator")
+pause & goto SubExecute
+goto:eof
+
+:subExecute
 MODE CON: lines= cols=50
 CLS & @Powershell.exe -File .\PINGLO~1.ps1
 goto:eof
